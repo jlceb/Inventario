56165b4ad83441add159408d108e1e27db82f748
jlceb <jlceballosf@gmail.com>
2018-02-08 13:13:52 -0400

    Modelo del backend y modelo de datos

---
+++ MyStock/MyStock.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyStock.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MyStock/MyStock.Domain/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStock.Domain
+{
+    public class Category
+    {
+        [Key]
+        public int IdCategory { get; set; }
+        [Required(ErrorMessage ="The field {0} is required.")]
+        [MaxLength(50,ErrorMessage ="The field {0} only can contain {1} characters length.")]
+        [Index("Category_Description_Index", IsUnique = true)]
+        public string Description { get; set; }
+    }
+}
